03ee687f3db45d5463dba0de52b9d46ba47f1ad8
DxxxxY <introsdream@gmail.com>
2022-07-27 01:58:42 -0400

    revamp: - change from gui to console - cool ass ascii title
    steps added:
    - git clone
    - setupdecompworkspace

--- src/main/java/studio/dreamys/Hephaestus.java
+++ src/main/java/studio/dreamys/Hephaestus.java
@@ -1,72 +1,61 @@
 package studio.dreamys;
 
-import javax.swing.*;
-import javax.swing.plaf.ColorUIResource;
-import javax.swing.plaf.FontUIResource;
+import org.eclipse.jgit.api.Git;
+import org.gradle.tooling.GradleConnector;
+import org.gradle.tooling.ProjectConnection;
 
-import studio.dreamys.gui.Panel;
-import studio.dreamys.gui.Window;
-import studio.dreamys.gui.sub.Button;
-import studio.dreamys.gui.sub.EditorPane;
-
-import java.awt.*;
-import java.io.IOException;
-import java.util.Enumeration;
-import java.util.Objects;
+import java.io.File;
+import java.net.URI;
 
 public class Hephaestus {
-    public static Font font;
+    public static File path = new File(System.getenv("APPDATA") + "\\Hephaestus");
 
     public static void main(String[] args) {
-        System.out.println("Hephaestus");
+        System.out.println(" /$$   /$$                     /$$                                       /$$                        \n" +
+                "| $$  | $$                    | $$                                      | $$                        \n" +
+                "| $$  | $$  /$$$$$$   /$$$$$$ | $$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$$ /$$$$$$   /$$   /$$  /$$$$$$$\n" +
+                "| $$$$$$$$ /$$__  $$ /$$__  $$| $$__  $$ |____  $$ /$$__  $$ /$$_____/|_  $$_/  | $$  | $$ /$$_____/\n" +
+                "| $$__  $$| $$$$$$$$| $$  \\ $$| $$  \\ $$  /$$$$$$$| $$$$$$$$|  $$$$$$   | $$    | $$  | $$|  $$$$$$ \n" +
+                "| $$  | $$| $$_____/| $$  | $$| $$  | $$ /$$__  $$| $$_____/ \\____  $$  | $$ /$$| $$  | $$ \\____  $$\n" +
+                "| $$  | $$|  $$$$$$$| $$$$$$$/| $$  | $$|  $$$$$$$|  $$$$$$$ /$$$$$$$/  |  $$$$/|  $$$$$$/ /$$$$$$$/\n" +
+                "|__/  |__/ \\_______/| $$____/ |__/  |__/ \\_______/ \\_______/|_______/    \\___/   \\______/ |_______/ \n" +
+                "                    | $$                                                                            \n" +
+                "                    | $$                                                                            \n" +
+                "                    |__/                                                                            ");
 
-        //theming
-        try {
-            GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(Font.createFont(Font.TRUETYPE_FONT, Objects.requireNonNull(Hephaestus.class.getResourceAsStream("/Rubik-Regular.ttf"))));
-        } catch (FontFormatException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        //clone repository
+        if (!path.exists()) {
+            log("Repository not found. Cloning repository...");
+
+            try {
+                Git.cloneRepository().setURI("https://github.com/DxxxxY/R.A.T.git").setDirectory(path).call();
+            } catch (Exception e) {
+                log("Error while cloning R.A.T repository.");
+                e.printStackTrace();
+                return;
+            }
+
+            log("Repository cloned successfully.");
+        } else {
+            log("Repository already exists. Skipping cloning.");
         }
 
-        font = new Font("Rubik-Regular", Font.PLAIN, 14);
+        //run setupDecompWorkspace
+        log("Running setupDecompWorkspace... (This may take a while)");
 
-        UIManager.put("Button.select", new ColorUIResource(Color.MAGENTA));
-        setUIFont(new FontUIResource(font));
+        try {
+            ProjectConnection connection = GradleConnector.newConnector().forProjectDirectory(path).useDistribution(new URI("https://services.gradle.org/distributions/gradle-3.1-bin.zip")).connect();
+            connection.newBuild().forTasks("setupDecompWorkspace").run();
+        } catch (Exception e) {
+            log("Error while running setupDecompWorkspace.");
+            e.printStackTrace();
+            return;
+        }
 
-        Window window = new Window("Hephaestus - R.A.T Forger");
-
-        Button button = new Button("Clone");
-
-        EditorPane editorPane = new EditorPane();
-        editorPane.setContentType("text/html");
-        editorPane.setText("<h1>Java</h1><p>is a general-purpose computer programming language that is       concurrent, class-based, object-oriented, and specifically designed to have as few          implementation dependencies as possible.</p>");
-
-        button.addActionListener(e -> {
-            button.setText("Cloning...");
-
-//            try {
-//                Git.cloneRepository().setURI("https://github.com/DxxxxY/R.A.T.git").setDirectory(new File(System.getenv("APPDATA") + "\\Hephaestus")).call();
-//            } catch (GitAPIException ex) {
-//                JOptionPane.showMessageDialog(null, "[Hephaestus] Error while cloning R.A.T repository.");
-//                throw new RuntimeException(ex);
-//            }
-
-            button.setText("Successfully cloned!");
-        });
-
-        window.add(button);
-        window.add(editorPane);
+        log("SetupDecompWorkspace finished successfully.");
     }
 
-    public static void setUIFont(FontUIResource f){
-        Enumeration<Object> keys = UIManager.getDefaults().keys();
-        while (keys.hasMoreElements()) {
-            Object key = keys.nextElement();
-            Object value = UIManager.get(key);
-            if (value instanceof FontUIResource) {
-                UIManager.put(key, f);
-            }
-        }
+    public static void log(String message) {
+        System.out.println("[Hephaestus] " + message);
     }
 }
